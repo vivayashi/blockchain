db0bfab795c04b8f51084e1701c34a7d20b8c2fd
Yutaro Obayashi <yutaro@YutaronoMacBook-puro.local>
2018-11-21 22:08:26 +0900

    Walletの作成

--- dev-test.js
+++ dev-test.js
@@ -1,7 +1,5 @@
-const Blockchain = require('./blockchain');
+const Wallet = require('./wallet');
 
-const bc = new Blockchain();
+const wallet = new Wallet();
 
-for (let i = 0; i<10; i++) {
-    console.log(bc.addBlock(`hello ${i}`).toString());
-}
+console.log(wallet.toString());
